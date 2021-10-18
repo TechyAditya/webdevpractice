2b9a5b3c932459b1128892a409cbeabdd857c733
TechyAditya <72063181+TechyAditya@users.noreply.github.com>
2021-10-18 10:43:00 +0530

    Cheems generator

--- js-codes/challenges/script.js
+++ js-codes/challenges/script.js
@@ -32,9 +32,48 @@ function ageInDays() {
     console.log(totalYears);
     console.log(totalMonths);
     console.log(totalDays);
+    /*
+    1. create element
+    2. generate text using text node
+    3. set current id attribute same as ageInDays function
+    4. append generated text to h1 child
+    5. append h1 child to the box by retrieving element using id method
+    */
     var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + totalDays + ' days old!');
+    var textAnswer = document.createTextNode('You are ' + totalDays + ' days old');
     h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
+}
+
+const Cheems = [
+    "https://github.com/adithyapaib/cheems/raw/master/Bike%20Cheems.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/AK47%20Cheems.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/Cute%20Doge.jpeg",
+    "https://github.com/adithyapaib/cheems/raw/master/Doge%20Lob.jpeg",
+    "https://github.com/adithyapaib/cheems/raw/master/bigandsmallcheems.png",
+    "https://github.com/adithyapaib/cheems/raw/master/cheems.png",
+    "https://github.com/adithyapaib/cheems/raw/master/cheems_bonk.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/cheems_oh.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/cheemscloud.png",
+    "https://github.com/adithyapaib/cheems/raw/master/cheemswithgun.png",
+    "https://github.com/adithyapaib/cheems/raw/master/coder%20memes.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/doge.png",
+    "https://github.com/adithyapaib/cheems/raw/master/dorime.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/tracksuitcheems.png",
+    "https://github.com/adithyapaib/cheems/raw/master/samuraicheems.png",
+    "https://github.com/adithyapaib/cheems/raw/master/images.jpeg",
+    "https://github.com/adithyapaib/cheems/raw/master/images%20(1).jpeg",
+    "https://github.com/adithyapaib/cheems/raw/master/human-dog.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/hey%20bhagwan.jpg",
+    "https://github.com/adithyapaib/cheems/raw/master/godzillavscheems.png",
+    "https://github.com/adithyapaib/cheems/raw/master/epiccheems.png"
+];
+var cheemNo = Math.floor(Math.random() * 10);
+
+function generateCheems() {
+    var image = document.createElement('img');
+    var div = document.getElementById('cheems-box-gen');
+    image.src = Cheems[cheemNo];
+    div.appendChild(image);
 }
